4e3df634a03a30756121717a6c64179df8800241
Saireddy <sainath.reddy@digite.com>
2024-03-14 15:20:55 +0530

    get namespace

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
 
 	"github.com/saireddyb/kubeide/pkg/client"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 func main() {
@@ -11,13 +16,36 @@ func main() {
 	kubeconfig := "/home/sainath_reddy/.kube/config"
 	
 	// Get the current context
-	context, _ := client.GetCurrentContext(kubeconfig)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// }
+	context, err := client.GetCurrentContext(kubeconfig)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 	fmt.Println("Current context:", context)
+
+	
+
+
+	clientset, err := client.Create(kubeconfig, context)
+	namespaces, err := ListNamespaces(clientset)
+	if err != nil {
+		fmt.Println(err.Error)
+		os.Exit(1)
+	}
+	for _, namespace := range namespaces.Items {
+		fmt.Println(namespace.Name)
+	}
+	fmt.Printf("Total namespaces: %d\n", len(namespaces.Items))
 
 }
 
 
+func ListNamespaces(client kubernetes.Interface) (*v1.NamespaceList, error) {
+	fmt.Println("Get Kubernetes Namespaces")
+	namespaces, err := client.CoreV1().Namespaces().List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		err = fmt.Errorf("error getting namespaces: %v", err)
+		return nil, err
+	}
+	return namespaces, nil
+}
 
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,6 +1,25 @@
 package client
 
-import "k8s.io/client-go/tools/clientcmd"
+import (
+	"strings"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+
+func Create(kubeconfig, context string) (*kubernetes.Clientset, error) {
+	config, err := GetConfig(kubeconfig, context)
+	if err != nil {
+		return nil, err
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return clientset, err
+}
 
 func GetCurrentContext(kubeconfig string) (string, error) {
 	config, err := clientcmd.LoadFromFile(kubeconfig)
@@ -9,4 +28,11 @@ func GetCurrentContext(kubeconfig string) (string, error) {
 	}
 	currentContext := config.CurrentContext
 	return currentContext, nil
+}
+
+func GetConfig(kubeconfig, context string) (*rest.Config, error) {
+	// use the current context in kubeconfig
+	return clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{Precedence: strings.Split(kubeconfig, ":")},
+		&clientcmd.ConfigOverrides{CurrentContext: context}).ClientConfig()
 }
